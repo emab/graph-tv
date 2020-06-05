e03045a4297b543883f3a7a6b304a1d422b6b1e7
Eddy Brown <eddybrown@me.com>
2020-06-05 17:18:38 +0100

    Add search bar

--- client/src/Search/index.js
+++ client/src/Search/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Search as SearchBar } from 'semantic-ui-react';
 
-const Search = () => {
+const Search = ({ setSelectedId }) => {
+  const [results, setResults] = useState([]);
+  const [value, setValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleResultSelect = (e, { result }) => {
+    setValue(result.title);
+    setSelectedId(result.id);
+  };
+
+  const handleSearchChange = (e, { value }) => {
+    setIsLoading(true);
+    setValue(value);
+  };
+
+  useEffect(() => {
+    const getResults = async (value) => {
+      const results = await fetch(`/api/search?q=${value}`);
+      const resultJson = await results.json();
+      setResults(resultJson);
+      setIsLoading(false);
+    };
+    getResults(value);
+  }, [value]);
+
   return (
-    <>
-      <p>Graph TV</p>
-    </>
+    <SearchBar
+      loading={isLoading}
+      onResultSelect={handleResultSelect}
+      onSearchChange={handleSearchChange}
+      results={results}
+      value={value}
+    />
   );
 };
 
