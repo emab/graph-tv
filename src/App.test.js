aa92f3c362d152c755ac236a680aa240a9d5a229
Eddy Brown <eddybrown@me.com>
2020-06-05 10:44:31 +0100

    Initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const p = getByText(/App/i);
+  expect(p).toBeInTheDocument();
 });
