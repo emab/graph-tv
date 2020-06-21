c935c06b2dd57319df6621f7a939de897a4d4c12
Eddy Brown <eddybrown@me.com>
2020-06-21 11:52:51 +0100

    Update graphdisplay

--- client/src/GraphDisplay/index.js
+++ client/src/GraphDisplay/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import Graph from '../Graph';
+import EpisodeGraph from '../Graph/EpisodeGraph';
+import SeasonGraph from '../Graph/SeasonGraph';
 
 const GraphDisplay = ({ data }) => {
   if (data) {
-    return Object.getOwnPropertyNames(data).map((sNumber) => {
-      return (
+    const seasonAverage = (seasonData) => {
+      let ratingTotal = 0;
+      seasonData.forEach((ep) => (ratingTotal += parseFloat(ep.rating)));
+      return ratingTotal / seasonData.length;
+    };
+    const getSeasonAverage = () => {
+      return Object.getOwnPropertyNames(data).map((season) => {
+        return { season: [season], rating: seasonAverage(data[season]) };
+      });
+    };
+
+    return (
+      <>
         <div>
-          <h2>Season {sNumber}</h2>
-          <Graph data={data[sNumber]} h={300} w={600} />
+          <h2>Season Ratings</h2>
+          <SeasonGraph data={getSeasonAverage()} h={300} w={600} />
         </div>
-      );
-    });
+        <div>
+          {Object.getOwnPropertyNames(data).map((sNumber) => {
+            return (
+              <div>
+                <h2>Season {sNumber}</h2>
+                <EpisodeGraph data={data[sNumber]} h={300} w={600} />
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
   } else {
     return 'No data';
   }
