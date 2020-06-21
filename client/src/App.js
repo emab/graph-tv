9b38e74aa5411c6c830344e3b88bd0930939ff5f
Eddy Brown <eddybrown@me.com>
2020-06-21 12:09:41 +0100

    Add loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Search from './Search';
 import GraphDisplay from './GraphDisplay';
-import { Button } from 'semantic-ui-react';
+import { Loader } from 'semantic-ui-react';
 
 const App = () => {
-  const [selectedId, setSelectedId] = useState('');
+  const [selectedId, setSelectedId] = useState(null);
   const [results, setResults] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setResults(null);
     const getAllRatings = async () => {
+      setLoading(true);
       const response = await fetch(`/api/ratings/${selectedId}`);
       const ratingsJson = await response.json();
       setResults(ratingsJson);
+      setLoading(false);
     };
-    getAllRatings();
-  }, [selectedId]);
-
-  const handleButtonOnClick = () => {
-    const westworldResults = {
-      '1': [
-        { episode: 1, rating: '8.9' },
-        { episode: 2, rating: '8.6' },
-        { episode: 3, rating: '8.4' },
-        { episode: 4, rating: '8.7' },
-        { episode: 5, rating: '8.7' },
-        { episode: 6, rating: '8.9' },
-        { episode: 7, rating: '9.5' },
-        { episode: 8, rating: '8.8' },
-        { episode: 9, rating: '9.4' },
-        { episode: 10, rating: '9.7' }
-      ],
-      '2': [
-        { episode: 1, rating: '8.2' },
-        { episode: 2, rating: '8.1' },
-        { episode: 3, rating: '8.0' },
-        { episode: 4, rating: '9.0' },
-        { episode: 5, rating: '7.9' },
-        { episode: 6, rating: '8.2' },
-        { episode: 7, rating: '8.6' },
-        { episode: 8, rating: '9.2' },
-        { episode: 9, rating: '8.8' },
-        { episode: 10, rating: '8.8' }
-      ],
-      '3': [
-        { episode: 1, rating: '8.4' },
-        { episode: 2, rating: '8.4' },
-        { episode: 3, rating: '8.4' },
-        { episode: 4, rating: '9.0' },
-        { episode: 5, rating: '8.2' },
-        { episode: 6, rating: '8.5' },
-        { episode: 7, rating: '8.1' },
-        { episode: 8, rating: '7.4' }
-      ]
+    if (selectedId) {
+      getAllRatings();
     }
-    setResults({});
-    setResults(westworldResults);
-    setSelectedId("tt0475784")
-  }
+  }, [selectedId]);
 
   return (
     <>
       <Search setSelectedId={setSelectedId} />
-      <Button onClick={handleButtonOnClick}>Set westworld results</Button>
-      <GraphDisplay data={results} />
+      {loading ? (
+        <Loader active inline="centered">
+          Loading IMDB Data...
+        </Loader>
+      ) : (
+        <GraphDisplay data={results} />
+      )}
     </>
   );
 };
