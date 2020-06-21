5b3141668133a79816cb61bd733472cadb9830ca
Eddy Brown <eddybrown@me.com>
2020-06-21 11:03:02 +0100

    Update server quotation format

--- server/api.js
+++ server/api.js
@@ -1,18 +1,26 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var imdb = require("js-imdb-scraper");
+var imdb = require('js-imdb-scraper');
 
 /* GET search results. */
-router.get("/search", (req, res, next) => {
+router.get('/search', (req, res, next) => {
   const show = req.query.q;
   search(show).then((showResults) => {
     res.send(showResults);
   });
 });
 
-router.get("/ratings/:showid", (req, res, next) => {
+router.get('/ratings/:showid', (req, res, next) => {
   const showId = req.params.showid;
   getAllRatings(showId).then((ratingResults) => {
+    res.send(ratingResults);
+  });
+});
+
+router.get('/ratings/:showid/:season', (req, res, next) => {
+  const showId = req.params.showid;
+  const season = req.params.season;
+  getSeasonRatings(showId, season).then((ratingResults) => {
     res.send(ratingResults);
   });
 });
@@ -24,6 +32,11 @@ const search = async (show) => {
 
 const getAllRatings = async (showId) => {
   const ratingResults = await imdb.getAllRatings(showId);
+  return ratingResults;
+};
+
+const getSeasonRatings = async (showId, season) => {
+  const ratingResults = await imdb.getSeasonRatings(showId, season);
   return ratingResults;
 };
 
