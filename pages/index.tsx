2008321c38e7283ef1d94b4a0f1ac9c2917c8659
Eddy Brown <edward.brown@yunojuno.com>
2023-02-26 12:44:49 +0000

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { useQuery } from 'react-query';
 import GraphDisplay, { Data } from '@/components/GraphDisplay';
 import cx from 'classnames';
 import { FaSadCry, FaGithub } from 'react-icons/fa';
+import Head from 'next/head';
 
 const getSeasonData = async (id: number | undefined) => {
   if (typeof id !== 'number') return Promise.resolve(null);
@@ -41,75 +42,84 @@ export default function Home() {
   );
 
   return (
-    <div className={cx(data ? 'bg-blue-900' : 'bg-neutral-50 h-full ')}>
-      <div className="fixed bottom-0 right-0 m-3 text-4xl">
-        <a
-          href="https://github.com/emab/graph-tv"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub />
-        </a>
-      </div>
-      <div className="bg-blue-900 py-5 flex flex-col items-center">
-        <h1 className="text-5xl text-white font-bold pb-5">Graph TV</h1>
-        <Search setSelected={setSelected} />
-      </div>
-      {data ? (
-        data.seasonEpisodeRatings.length ? (
-          <>
-            <div className="bg-blue-200 p-10 text-xl text-center">
-              <p>
-                You&apos;ve selected{' '}
-                <span className="font-bold">{selected?.name}</span>.
-              </p>
-              <p>
-                It has{' '}
-                <span className="font-bold">{episodeCount} episodes</span> over{' '}
-                <span className="font-bold">
-                  {data.seasonAverageRatings.length}
-                </span>{' '}
-                seasons with an overall rating of{' '}
-                <span className="font-bold">
-                  {selected?.rating?.toFixed(2)}
-                </span>
-                !
+    <>
+      <Head>
+        <title>Graph TV</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <div className={cx(data ? 'bg-blue-900' : 'bg-neutral-50 h-full ')}>
+        <div className="fixed bottom-0 right-0 m-3 text-4xl">
+          <a
+            href="https://github.com/emab/graph-tv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </div>
+        <div className="bg-blue-900 py-5 flex flex-col items-center">
+          <h1 className="text-5xl text-white font-bold pb-5">Graph TV</h1>
+          <Search setSelected={setSelected} />
+        </div>
+        {data ? (
+          data.seasonEpisodeRatings.length ? (
+            <>
+              <div className="bg-blue-200 p-10 text-xl text-center">
+                <p>
+                  You&apos;ve selected{' '}
+                  <span className="font-bold">{selected?.name}</span>.
+                </p>
+                <p>
+                  It has{' '}
+                  <span className="font-bold">{episodeCount} episodes</span>{' '}
+                  over{' '}
+                  <span className="font-bold">
+                    {data.seasonAverageRatings.length}
+                  </span>{' '}
+                  seasons with an overall rating of{' '}
+                  <span className="font-bold">
+                    {selected?.rating?.toFixed(2)}
+                  </span>
+                  !
+                </p>
+              </div>
+              <GraphDisplay data={data} />
+            </>
+          ) : (
+            <div className="p-10 text-center">
+              <p className="text-3xl text-white">Oh no!</p>
+              <div className="flex justify-center text-6xl my-3 text-yellow-400">
+                <FaSadCry />
+              </div>
+              <p className="text-xl text-white">
+                There wasn&apos;t enough data to show graphs for that show. Try
+                another one!
               </p>
             </div>
-            <GraphDisplay data={data} />
-          </>
+          )
         ) : (
           <div className="p-10 text-center">
-            <p className="text-3xl text-white">Oh no!</p>
-            <div className="flex justify-center text-6xl my-3 text-yellow-400">
-              <FaSadCry />
-            </div>
-            <p className="text-xl text-white">
-              There wasn&apos;t enough data to show graphs for that show. Try
-              another one!
+            <p className="text-xl">Hey and welcome to GraphTV!</p>
+            <p className="mt-4">
+              To get started, enter the name of a show in the search bar above.
+            </p>
+            <p>
+              Search and ratings are{' '}
+              <a
+                className="underline text-blue-500 hover:text-blue-600"
+                href="https://www.themoviedb.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                powered by TMDB
+              </a>
+              .
             </p>
           </div>
-        )
-      ) : (
-        <div className="p-10 text-center">
-          <p className="text-xl">Hey and welcome to GraphTV!</p>
-          <p className="mt-4">
-            To get started, enter the name of a show in the search bar above.
-          </p>
-          <p>
-            Search and ratings are{' '}
-            <a
-              className="underline text-blue-500 hover:text-blue-600"
-              href="https://www.themoviedb.org/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              powered by TMDB
-            </a>
-            .
-          </p>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
