cc1d0c875f5b45dbb408288b2b8ed2683014e239
Eddy Brown <edward.brown@yunojuno.com>
2023-03-10 23:15:31 +0000

    Improve loading screen

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,5 +1,10 @@
+import { CgSpinner } from 'react-icons/cg';
+
 export const Loading = () => (
   <div className="p-10 text-center">
-    <h3 className="text-2xl">Hang on a sec! Grabbing the data for you...</h3>
+    <h3 className="text-2xl">Hang on! Grabbing data from TMDB...</h3>
+    <div className="flex justify-center mt-5 text-4xl">
+      <CgSpinner className="animate-spin" />
+    </div>
   </div>
 );
