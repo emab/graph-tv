6000a1f9725a3da3e3575813e786e39b3b7b1fc6
Eddy Brown <eddybrown@me.com>
2023-03-12 10:51:36 +0000

    Move <Head /> component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,50 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useState } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} /> <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Graph TV</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="View a ratings graph of your favourite TV shows"
+        />
+      </Head>
+      <Script
+        id="heap"
+        type="application/javascript"
+      >{`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("3553486198");`}</Script>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />{' '}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import * as d3 from 'd3';
 import { Search } from '@/components/Search';
 import { useQuery } from 'react-query';
 import GraphDisplay, { Data } from '@/components/GraphDisplay';
-import Head from 'next/head';
 import { FloatingLinks } from '@/components/FloatingLinks';
 import { Welcome } from '@/components/Welcome';
 import { Loading } from '@/components/Loading';
@@ -69,34 +68,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Graph TV</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          name="description"
-          content="View a ratings graph of your favourite TV shows"
-        />
-      </Head>
       <div className="bg-gray-700 min-h-full">
         <FloatingLinks />
         <div
