aafef7115478d3c7f71fca188b9ce357aab03336
Eddy Brown <eddybrown@me.com>
2023-03-12 10:26:39 +0000

    Add heap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        id="heap"
+        type="application/javascript"
+      >{`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("3553486198");`}</Script>
       <body>
         <Main />
         <NextScript />
